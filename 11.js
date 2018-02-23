4dd9533d689cbf7450495165d0194f5fb4b4519d
manjabogicevic <slavo@mimicom24.com>
2018-02-23 18:35:54 -0500

    code is working

--- 11.js
+++ 11.js
@@ -1,3 +1,11 @@
 // max(...nums) ⇒ number
 // Write a function max
 // that is generalized for any amount of arguments
+
+function max(...nums) {
+  return nums.reduce((total, num) => {
+    return total > num ? total : num;
+  });
+}
+
+console.log(max(1, 4, 89, 354, 3, 5, 6));
