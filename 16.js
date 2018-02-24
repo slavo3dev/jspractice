8afc7164143b9d2d49ba070fd0bbfd8b5d3cbe49
manjabogicevic <slavo@mimicom24.com>
2018-02-23 19:03:05 -0500

    adding func

--- 16.js
+++ 16.js
@@ -1,1 +1,8 @@
-// Write a function not that takes a function and returns the negation of its result
+// Write a function not
+// that takes a function and returns the negation of its result
+
+function not(func) {
+  return function(...args) {
+    return !func(...args);
+  };
+}
