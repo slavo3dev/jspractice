2dfa3e69eec04c6496813c19f972df09da33d804
manjabogicevic <slavo@mimicom24.com>
2018-03-17 13:28:05 -0400

    adding variable

--- t.js
+++ t.js
@@ -1,12 +1,12 @@
 let t = ["s", "l", "a", "v"];
 let name = "slavo";
 let l = [];
-
+var j = 0;
 for (var i = 0; i < t.length; i++) {
   for (j = 1; j < t.length; j++) {
     console.log(t[i]);
     console.log(t[j]);
     console.log("----------");
   }
-  t.slice();
+  j++;
 }
