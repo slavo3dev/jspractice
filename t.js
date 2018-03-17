221ee87d80889da6fb8a2379d299afe5d18563dd
manjabogicevic <slavo@mimicom24.com>
2018-03-17 12:48:00 -0400

    slice method

--- t.js
+++ t.js
@@ -1,23 +1,3 @@
 let t = ["s", "l", "a", "v"];
-let x = "slavo";
 
-// for (var i = 0; i < t.length; i++) {
-//   for (var j = 1; j < t.length; j++) {
-//     console.log(t[i]);
-//     console.log(t[j]);
-//     console.log(t[i] === t[j]);
-//     console.log('----------------')
-//   }
-// }
-
-for (var i = 0; i < t.length; i++) {
-  if (x.includes(t[i])) {
-    console.log(true);
-  } else {
-    console.log(false);
-  }
-}
-
-let a = x.includes("s");
-
-console.log(a);
+console.log(t.slice(1));
