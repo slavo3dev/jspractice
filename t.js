37b0750d1965bd862776214278006f285b05460c
manjabogicevic <slavo@mimicom24.com>
2018-03-17 11:45:20 -0400

    filter question

--- t.js
+++ t.js
@@ -1,7 +1,7 @@
-t = ["s", "l", "a", "v"];
+let t = ["s", "l", "a", "v"];
 
 for (var i = 0; i < t.length; i++) {
-  for (var j = 1; j < t.length; j++) {
+  for (var j = 1; j < t.length - 1; j++) {
     console.log(t[i] === t[j]);
   }
 }
