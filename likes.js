b94592337bbfca20d9f775515e848bd8606bef95
manjabogicevic <slavo@mimicom24.com>
2018-03-17 18:42:44 -0400

    adduing ()

--- likes.js
+++ likes.js
@@ -5,3 +5,9 @@
 function likes(names) {
   // TODO
 }
+
+likes([]); // must be "no one likes this"
+likes(["Peter"]); // must be "Peter likes this"
+likes(["Jacob", "Alex"]); // must be "Jacob and Alex like this"
+likes(["Max", "John", "Mark"]); // must be "Max, John and Mark like this"
+likes(["Alex", "Jacob", "Mark", "Max"]); // must be "Alex, Jacob and 2 others like this"
