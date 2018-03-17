98eaabc6f9a958bfc6caab15c8ac754a81e07e21
manjabogicevic <slavo@mimicom24.com>
2018-03-17 18:47:58 -0400

    conditional statement

--- likes.js
+++ likes.js
@@ -4,15 +4,15 @@
 
 function likes(names) {
   // TODO
-  if (names.length < 0) {
+  if (names.length === 0) {
     return "no one likes me";
   }
 
   return true;
 }
 
-likes([]); // must be "no one likes this"
-likes(["Peter"]); // must be "Peter likes this"
-likes(["Jacob", "Alex"]); // must be "Jacob and Alex like this"
-likes(["Max", "John", "Mark"]); // must be "Max, John and Mark like this"
-likes(["Alex", "Jacob", "Mark", "Max"]); // must be "Alex, Jacob and 2 others like this"
+console.log(likes([])); // must be "no one likes this"
+console.log(likes(["Peter"])); // must be "Peter likes this"
+console.log(likes(["Jacob", "Alex"])); // must be "Jacob and Alex like this"
+console.log(likes(["Max", "John", "Mark"])); // must be "Max, John and Mark like this"
+console.log(likes(["Alex", "Jacob", "Mark", "Max"])); // must be "Alex, Jacob and 2 others like this"
