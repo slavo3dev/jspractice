0abc15fc554394806e9b953c609a9b9d7201b884
manjabogicevic <slavo@mimicom24.com>
2018-03-17 13:36:58 -0400

    close to done

--- t.js
+++ t.js
@@ -3,11 +3,12 @@ let name = "slavo";
 let l = [];
 var j = 0;
 for (var i = 0; i < t.length; i++) {
-  for (j = 1; j < t.length; j++) {
-    console.log(t[j]);
+  for (j = 1 + i; j < t.length; j++) {
     console.log(t[i]);
+    console.log(t[j]);
     console.log("Inside");
     console.log("----------");
   }
   console.log("Outside");
+  console.log("------ The End ---------");
 }
