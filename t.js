4e545a4a47e8f10797296b4461ffb5d190225785
manjabogicevic <slavo@mimicom24.com>
2018-03-17 12:24:28 -0400

    test

--- t.js
+++ t.js
@@ -1,7 +1,10 @@
 let t = ["s", "l", "a", "v"];
 
 for (var i = 0; i < t.length; i++) {
-  for (var j = 1; j < t.length - 1; j++) {
+  for (var j = 1; j < t.length; j++) {
+    console.log(t[i]);
+    console.log(t[j]);
     console.log(t[i] === t[j]);
+    console.log('----------------')
   }
 }
