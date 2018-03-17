8951726c49855566e85ddcd003891f548860f9d3
manjabogicevic <slavo@mimicom24.com>
2018-03-17 19:22:57 -0400

    testing for loop

---
+++ for.js
@@ -0,0 +1,9 @@
+let list = ["slavo", "nenad", "marko", "vedran", "zoran", "predrag"];
+let count = 0;
+
+for (var i = 3; i < list.length; i++) {
+  console.log(list[i]);
+  count++;
+}
+
+console.log(count);
