281e2ae88101de5fa4fbb70e0ab0adf4984e99c4
manjabogicevic <slavo@mimicom24.com>
2018-03-17 12:32:47 -0400

    adding else if statement

--- t.js
+++ t.js
@@ -1,10 +1,23 @@
 let t = ["s", "l", "a", "v"];
+let x = "slavo";
+
+// for (var i = 0; i < t.length; i++) {
+//   for (var j = 1; j < t.length; j++) {
+//     console.log(t[i]);
+//     console.log(t[j]);
+//     console.log(t[i] === t[j]);
+//     console.log('----------------')
+//   }
+// }
 
 for (var i = 0; i < t.length; i++) {
-  for (var j = 1; j < t.length; j++) {
-    console.log(t[i]);
-    console.log(t[j]);
-    console.log(t[i] === t[j]);
-    console.log('----------------')
+  if (x.includes(t[i])) {
+    console.log(true);
+  } else {
+    console.log(false);
   }
 }
+
+let a = x.includes("s");
+
+console.log(a);
