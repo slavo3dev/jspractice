9ee52d794a003e568f4be6cebd05a27ae5e00fae
manjabogicevic <slavo@mimicom24.com>
2018-03-18 21:07:05 -0400

    adding

---
+++ max.js
@@ -0,0 +1,14 @@
+s1 = [
+  "hoqq",
+  "bbllkw",
+  "oox",
+  "ejjuyyy",
+  "plmiis",
+  "xxxzgpsssa",
+  "xxwwkktt",
+  "znnnnfqknaz",
+  "qqquuhii",
+  "dvvvwz"
+];
+
+console.log(s1.Max(x => x.length));
