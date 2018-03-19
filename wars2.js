c376c6f6b1488163310108d6dc783f35f010c356
manjabogicevic <slavo@mimicom24.com>
2018-03-18 20:57:18 -0400

    code is working

--- wars2.js
+++ wars2.js
@@ -5,7 +5,11 @@
 // If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
 
 function mxdiflg(a1, a2) {
-  // your code
+  if (a1.length === 0 || a2.length === 0) {
+    return -1;
+  }
+
+  return "If statement is working!!!";
 }
 
 s1 = [
@@ -22,4 +26,7 @@ s1 = [
 ];
 s2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"];
 
-mxdiflg(s1, s2); // 13
+l1 = [];
+l2 = ["Slavo", "Marko", "Jovan"];
+console.log(mxdiflg(s1, s2)); // 13
+console.log(mxdiflg(l1, l2)); // -1
