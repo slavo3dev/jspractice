82e344eb5170707445df740cd19434e9636df97e
manjabogicevic <slavo@mimicom24.com>
2018-03-27 23:05:30 -0400

    console.log the output

--- wars4.js
+++ wars4.js
@@ -4,8 +4,10 @@ function reverseWords(str) {
   return str
     .split(" ")
     .reverse()
-    .join(" "); // reverse those words
+    .join(" ");
 }
 
-reverseWords("The greatest victory is that which requires no battle");
+console.log(
+  reverseWords("The greatest victory is that which requires no battle")
+);
 // should return "battle no requires which that is victory greatest The"
