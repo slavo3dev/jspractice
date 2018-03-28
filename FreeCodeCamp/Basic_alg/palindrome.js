81e8adbda69930f0fbb16e0926520643dbd49a51
manjabogicevic <slavo@mimicom24.com>
2018-03-28 00:50:14 -0400

    palindrom

--- FreeCodeCamp/Basic_alg/palindrome.js
+++ FreeCodeCamp/Basic_alg/palindrome.js
@@ -8,3 +8,10 @@
 // We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
 
 // We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2".
+
+function palindrome(str) {
+  // Good luck!
+  return true;
+}
+
+palindrome("eye");
