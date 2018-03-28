22a626e4411640adc682cde9e52451537f598ee2
manjabogicevic <slavo@mimicom24.com>
2018-03-28 00:33:02 -0400

    adding

--- FreeCodeCamp/one.js
+++ FreeCodeCamp/one.js
@@ -3,11 +3,21 @@
 // The lowest number will not always come first.
 
 function sumAll(arr) {
-  let a = [];
-  for (let i = arr[0]; arr[0] <= arr[1]; i++) {
+  var a = [];
+  if (arr[0] < arr[1]) {
+    for (var i = arr[0]; i <= arr[1]; i++) {
+      a.push(i);
+    }
+  }
+  for (var i = arr[1]; i <= arr[0]; i++) {
     a.push(i);
   }
   return a.reduce((a, b) => a + b);
 }
 
 console.log(sumAll([1, 4]));
+
+console.log(sumAll([1, 4]));
+console.log(sumAll([1, 4])); // should return 10.
+console.log(sumAll([4, 1])); // should return 10.
+console.log(sumAll([5, 10])); // should return 45.
