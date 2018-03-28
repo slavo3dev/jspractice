141bc9ac6e1189c75b9f6ae40cec0e3907cbcd3d
manjabogicevic <slavo@mimicom24.com>
2018-03-27 23:04:31 -0400

    join

---
+++ t4.js
@@ -0,0 +1,7 @@
+let str = "The greatest victory is that which requires no battle";
+str = str
+  .split(" ")
+  .reverse()
+  .join(" ");
+
+console.log(str);
--- wars4.js
+++ wars4.js
@@ -1,8 +1,11 @@
 // Complete the solution so that it reverses all of the words within the string passed in.
 
-function reverseWords(str){
-    return str; // reverse those words
-  }
+function reverseWords(str) {
+  return str
+    .split(" ")
+    .reverse()
+    .join(" "); // reverse those words
+}
 
-reverseWords("The greatest victory is that which requires no battle")
+reverseWords("The greatest victory is that which requires no battle");
 // should return "battle no requires which that is victory greatest The"
