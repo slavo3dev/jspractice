9605428696b496739be0dd492e9b19e03971068c
manjabogicevic <slavo@mimicom24.com>
2018-03-28 00:36:27 -0400

    reverse string

--- FreeCodeCamp/Basic_alg/reverse.js
+++ FreeCodeCamp/Basic_alg/reverse.js
@@ -3,3 +3,12 @@
 // You may need to turn the string into an array before you can reverse it.
 
 // Your result must be a string.
+
+function reverseString(str) {
+  return str
+    .split(" ")
+    .revese()
+    .join("");
+}
+
+reverseString("hello");
