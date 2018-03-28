38900bfc3054f8fa49db80bbff68e142f294b57a
manjabogicevic <slavo@mimicom24.com>
2018-03-27 23:00:40 -0400

    starting practicing

---
+++ wars4.js
@@ -0,0 +1,8 @@
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+function reverseWords(str){
+    return str; // reverse those words
+  }
+
+reverseWords("The greatest victory is that which requires no battle")
+// should return "battle no requires which that is victory greatest The"
