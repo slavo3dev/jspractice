578aabe78fe550dd80870b509dc489b1d07f9b72
manjabogicevic <slavo@mimicom24.com>
2018-03-29 22:48:20 -0400

    adding newArr

--- t7.js
+++ t7.js
@@ -6,3 +6,31 @@ str.split(" ").forEach(item => {
 });
 
 console.log(str_new.join(" "));
+
+console.log("---------------------");
+let arr1 = [1, 2, 3, 5];
+let arr2 = [1, 2, 3, 4, 5];
+let newArr = [];
+
+for (var i = 0; i < arr1.length; i++) {
+  for (var j = 0; j < arr2.length; j++) {
+    if (arr1[i] !== arr2[j]) {
+      newArr.push(arr1[i]);
+      newArr.push(arr2[j]);
+    }
+  }
+}
+
+console.log(newArr);
+// const words = [
+//   "spray",
+//   "limit",
+//   "elite",
+//   "exuberant",
+//   "destruction",
+//   "present",
+//   "happy"
+// ];
+
+// let longWords = words.filter(word => word.length > 6);
+// console.log(longWords);
