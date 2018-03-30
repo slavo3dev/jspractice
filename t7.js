10c347486ea150aac7b9ceed2fd79fe1d8203469
manjabogicevic <slavo@mimicom24.com>
2018-03-30 00:29:59 -0400

    testing loop

--- t7.js
+++ t7.js
@@ -8,35 +8,32 @@ str.split(" ").forEach(item => {
 console.log(str_new.join(" "));
 
 console.log("---------------------");
-let arr1 = [1, 2, 3, 5];
-let arr2 = [1, 2, 3, 4, 5];
+// let arr1 = [1, 2, 3, 5];
+// let arr2 = [1, 2, 3, 4, 5];
+let arr1 = [1, "calf", 3, "piglet"];
+let arr2 = [7, "filly"];
+
 let newArr = [];
 
-newArr = arr1.concat(arr2);
-
-for (var i = 0; i < arr1.lenght; i++) {
-  for (var j = 0; j < arr2.lenght; j++) {
-    if (arr1[i] === arr2[j]) {
-      arr[i].splice(i, 1);
-      arr2[i].splice(i, 1);
-      break;
+if (arr1.length > arr2.length) {
+  for (var i = 0; i < arr1.length; i++) {
+    for (var j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        console.log(arr1.splice(i, 1));
+      }
+    }
+  }
+} else {
+  for (var i = 0; i < arr2.length; i++) {
+    for (var j = 0; j < arr1.length; j++) {
+      if (arr2[i] === arr1[j]) {
+        console.log(arr2.splice(i, 1));
+      }
     }
   }
 }
-
-console.log(arr1);
-console.log(arr2);
-
-// console.log(newArr.sort());
-// const words = [
-//   "spray",
-//   "limit",
-//   "elite",
-//   "exuberant",
-//   "destruction",
-//   "present",
-//   "happy"
-// ];
-
-// let longWords = words.filter(word => word.length > 6);
-// console.log(longWords);
+newArr = arr1.concat(arr2);
+console.log(newArr);
+// console.log(newArr.sort()); const words = [   "spray",   "limit",   "elite",
+//  "exuberant",   "destruction",   "present",   "happy" ]; let longWords =
+// words.filter(word => word.length > 6); console.log(longWords);
---
+++ t8.js
@@ -0,0 +1,31 @@
+let arr1 = [1, 2, 3, 5];
+let arr2 = [1, 2, 3, 4, 5];
+let newArr = [];
+
+// for (var i = 0; i < arr2.length; i++) {
+//     for (var j = 0; j < arr1.length; j++) {
+//       if (arr2[i] === arr1[j]) {
+//         console.log(arr2[i]);
+//         console.log(arr1[j]);
+//         console.log("=============");
+//         arr2.splice(i, 1);
+//         arr1.splice(j, 1);
+//       }
+//     }
+//   }
+
+for (var i = 0; i < arr2.length; i++) {
+  for (var j = 0; j < arr1.length; j++) {
+    if (arr2[i] === arr1[j]) {
+      console.log(arr2[i]);
+      console.log(arr1[j]);
+      console.log("======================");
+      console.log(arr2.splice(i, 1));
+      console.log(arr1.splice(j, 1));
+    }
+    console.log("***************************");
+  }
+}
+console.log(arr1);
+console.log(arr2);
+console.log(arr1.concat(arr2));
