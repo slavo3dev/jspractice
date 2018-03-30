903de6b48f3f0c6eebdbe6b1176fe04d292cba17
manjabogicevic <slavo@mimicom24.com>
2018-03-29 22:53:21 -0400

    concat

--- t7.js
+++ t7.js
@@ -12,16 +12,9 @@ let arr1 = [1, 2, 3, 5];
 let arr2 = [1, 2, 3, 4, 5];
 let newArr = [];
 
-for (var i = 0; i < arr1.length; i++) {
-  for (var j = 0; j < arr2.length; j++) {
-    if (arr1[i] !== arr2[j]) {
-      newArr.push(arr1[i]);
-      newArr.push(arr2[j]);
-    }
-  }
-}
+newArr = arr1.concat(arr2);
 
-console.log(newArr);
+console.log(newArr.sort());
 // const words = [
 //   "spray",
 //   "limit",
