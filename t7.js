9cb9ffdc3fe2a2907caff329c953d493fae8cebb
manjabogicevic <slavo@mimicom24.com>
2018-03-29 23:37:33 -0400

    fixing arrays names

--- t7.js
+++ t7.js
@@ -14,7 +14,20 @@ let newArr = [];
 
 newArr = arr1.concat(arr2);
 
-console.log(newArr.sort());
+for (var i = 0; i < arr1.lenght; i++) {
+  for (var j = 0; j < arr2.lenght; j++) {
+    if (arr1[i] === arr2[j]) {
+      arr[i].splice(i, 1);
+      arr2[i].splice(i, 1);
+      break;
+    }
+  }
+}
+
+console.log(arr1);
+console.log(arr2);
+
+// console.log(newArr.sort());
 // const words = [
 //   "spray",
 //   "limit",
