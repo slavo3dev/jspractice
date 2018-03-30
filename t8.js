a8d9d80ddbe00ba4b1aae0e3033917bf5db1b420
manjabogicevic <slavo@mimicom24.com>
2018-03-30 00:46:40 -0400

    done

--- t8.js
+++ t8.js
@@ -1,4 +1,4 @@
-let arr1 = [1, 2, 3, 5];
+let arr1 = [1, 2, 3, 4];
 let arr2 = [1, 2, 3, 4, 5];
 let newArr = [];
 
@@ -15,7 +15,7 @@ let newArr = [];
 //   }
 
 for (var i = 0; i < arr2.length; i++) {
-  for (var j = 0; j < arr1.length; j++) {
+  for (var j = 1; j < arr1.length; j++) {
     if (arr2[i] === arr1[j]) {
       console.log(arr2[i]);
       console.log(arr1[j]);
@@ -25,6 +25,8 @@ for (var i = 0; i < arr2.length; i++) {
     }
     console.log("***************************");
   }
+
+  i = i - 1;
 }
 console.log(arr1);
 console.log(arr2);
