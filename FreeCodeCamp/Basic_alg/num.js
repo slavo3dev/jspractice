0d3c68bb5c85ebca573d8cc0b768f1113999659f
manjabogicevic <slavo@mimicom24.com>
2018-03-31 18:32:44 -0400

    adidng cosole.log

--- FreeCodeCamp/Basic_alg/num.js
+++ FreeCodeCamp/Basic_alg/num.js
@@ -1,2 +1,13 @@
 // Repeat a given string (first argument) num times (second argument). Return an empty string if num is not a positive number.
 
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  return str;
+}
+
+console.log(repeatStringNumTimes("*", 3)); // should return "***".
+console.log(repeatStringNumTimes("abc", 3)); // should return "abcabcabc".
+console.log(repeatStringNumTimes("abc", 4)); //  should return "abcabcabcabc".
+console.log(repeatStringNumTimes("abc", 1)); // should return "abc".
+console.log(repeatStringNumTimes("*", 8)); //  should return "********".
+console.log(repeatStringNumTimes("abc", -2)); // should return "".
