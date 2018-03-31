7ccca89323410a8d00a076a378031520fec481a6
manjabogicevic <slavo@mimicom24.com>
2018-03-31 00:36:24 -0400

    fixing code

--- FreeCodeCamp/Basic_alg/ending.js
+++ FreeCodeCamp/Basic_alg/ending.js
@@ -3,20 +3,18 @@
 // This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
 
 function confirmEnding(str, target) {
-  return str.substr(-target.length) === target ? true : false;
-}
-
-console.log(confirmEnding("Bastian", "n"));
+    return str.substr(-target.length) === target ? true : false;
+  }
 
 
-console.log(confirmEnding("Bastian", "n")) // should return true.
-console.log(confirmEnding("Connor", "n"))  // should return false.
-console.log(confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification")) // should return false.
-console.log(confirmEnding("He has to give me a new name", "name")) // should return true.
-console.log(confirmEnding("Open sesame", "same")) // should return true.
-console.log(confirmEnding("Open sesame", "pen")) // should return false.
-console.log(confirmEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain")) // should return false.
-// Do not use the built-in method .endsWith() to solve the challenge.
+console.log(confirmEnding("Bastian", "n")); // should return true.
+console.log(confirmEnding("Connor", "n"));  // should return false.
+console.log(confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification")); // should return false.
+console.log(confirmEnding("He has to give me a new name", "name")); // should return true.
+console.log(confirmEnding("Open sesame", "same")); // should return true.
+console.log(confirmEnding("Open sesame", "pen")); // should return false.
+console.log(confirmEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain")); // should return false.
+
 
 
 ​
