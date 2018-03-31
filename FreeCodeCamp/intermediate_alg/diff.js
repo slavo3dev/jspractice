cf8b55694a64dc4ba9734923c7db3674d035301f
manjabogicevic <slavo@mimicom24.com>
2018-03-30 21:37:18 -0400

    included

--- FreeCodeCamp/intermediate_alg/diff.js
+++ FreeCodeCamp/intermediate_alg/diff.js
@@ -3,38 +3,31 @@
 // difference of the two arrays.
 
 function diffArray(arr1, arr2) {
-    var newArr = [];
-
-    for (word in arr1) {
-        newArr = arr2.filter(item => item === arr1[word]);
-    }
-    return newArr;
+  return arr1
+    .concat(arr2)
+    .filter(item => !arr1.includes(item) || !arr2.includes(item));
 }
 
-console.log(diffArray([
-    1, 2, 3, 5
-], [1, 2, 3, 4, 5])); // should return an array.
-console.log(diffArray([
-    "diorite",
-    "andesite",
-    "grass",
-    "dirt",
-    "pink wool",
-    "dead shrub"
-], ["diorite", "andesite", "grass", "dirt", "dead shrub"])); // should return ["pink wool"].
-console.log(diffArray([
-    "andesite", "grass", "dirt", "pink wool", "dead shrub"
-], ["diorite", "andesite", "grass", "dirt", "dead shrub"])); // should return ["diorite", "pink wool"].
-console.log(diffArray([
-    "andesite", "grass", "dirt", "dead shrub"
-], ["andesite", "grass", "dirt", "dead shrub"])); // should return [].
-console.log(diffArray([
-    1, 2, 3, 5
-], [1, 2, 3, 4, 5])); //should return [4].
-console.log(diffArray([
-    1, "calf", 3, "piglet"
-], [1, "calf", 3, 4])); // should return ["piglet", 4].
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5])); // should return an array.
+console.log(
+  diffArray(
+    ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"],
+    ["diorite", "andesite", "grass", "dirt", "dead shrub"]
+  )
+); // should return ["pink wool"].
+console.log(
+  diffArray(
+    ["andesite", "grass", "dirt", "pink wool", "dead shrub"],
+    ["diorite", "andesite", "grass", "dirt", "dead shrub"]
+  )
+); // should return ["diorite", "pink wool"].
+console.log(
+  diffArray(
+    ["andesite", "grass", "dirt", "dead shrub"],
+    ["andesite", "grass", "dirt", "dead shrub"]
+  )
+); // should return [].
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5])); //should return [4].
+console.log(diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4])); // should return ["piglet", 4].
 console.log(diffArray([], ["snuffleupagus", "cookie monster", "elmo"])); // should return ["snuffleupagus", "cookie monster", "elmo"].
-console.log(diffArray([
-    1, "calf", 3, "piglet"
-], [7, "filly"])); // should return [1, "calf", 3, "piglet", 7, "filly"].
+console.log(diffArray([1, "calf", 3, "piglet"], [7, "filly"])); // should return [1, "calf", 3, "piglet", 7, "filly"].
--- t8.js
+++ t8.js
@@ -1,6 +1,10 @@
-let arr1 = [1, 2, 3, 4];
-let arr2 = [1, 2, 3, 4, 5];
+// let arr1 = [1, 2, 3, 4];
+// let arr2 = [1, 2, 3, 4, 5];
 let newArr = [];
+
+let arr2 = ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"];
+
+let arr1 = ["diorite", "andesite", "grass", "dirt", "dead shrub"];
 
 // for (var i = 0; i < arr2.length; i++) {
 //     for (var j = 0; j < arr1.length; j++) {
@@ -13,21 +17,26 @@ let newArr = [];
 //       }
 //     }
 //   }
-
-for (var i = 0; i < arr2.length; i++) {
-  for (var j = 1; j < arr1.length; j++) {
-    if (arr2[i] === arr1[j]) {
-      console.log(arr2[i]);
-      console.log(arr1[j]);
-      console.log("======================");
-      console.log(arr2.splice(i, 1));
-      console.log(arr1.splice(j, 1));
+if (arr2.length > arr1.length) {
+  for (var i = 0; i < arr2.length; i++) {
+    for (var j = 0; j < arr1.length; j++) {
+      if (arr2[i] === arr1[j]) {
+        arr2.splice(i, 1);
+      }
     }
-    console.log("***************************");
   }
-
-  i = i - 1;
+} else {
+  for (var i = 0; i < arr1.length; i++) {
+    for (var j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        arr1.splice(i, 1);
+      }
+    }
+  }
 }
-console.log(arr1);
+
+// console.log(arr1);
 console.log(arr2);
-console.log(arr1.concat(arr2));
+// console.log(arr1.concat(arr2));
+console.log(newArr.concat(arr2));
+console.log(newArr.concat(arr1));
