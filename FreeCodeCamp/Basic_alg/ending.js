d12f87782016e1988f5dbcb34f25af4b1ef2fb59
manjabogicevic <slavo@mimicom24.com>
2018-03-31 00:17:45 -0400

    substring

--- FreeCodeCamp/Basic_alg/ending.js
+++ FreeCodeCamp/Basic_alg/ending.js
@@ -3,9 +3,20 @@
 // This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
 
 function confirmEnding(str, target) {
-  // "Never give up and good luck will find you."
-  // -- Falcor
   return str;
 }
 
-confirmEnding("Bastian", "n");
+console.log(confirmEnding("Bastian", "n"));
+
+
+console.log(confirmEnding("Bastian", "n")) // should return true.
+console.log(confirmEnding("Connor", "n"))  // should return false.
+console.log(confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification")) // should return false.
+console.log(confirmEnding("He has to give me a new name", "name")) // should return true.
+console.log(confirmEnding("Open sesame", "same")) // should return true.
+console.log(confirmEnding("Open sesame", "pen")) // should return false.
+console.log(confirmEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain")) // should return false.
+// Do not use the built-in method .endsWith() to solve the challenge.
+
+
+​
