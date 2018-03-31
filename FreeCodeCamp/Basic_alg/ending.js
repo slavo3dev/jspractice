538fe4c8921e64043e67d2b4db9da8fa10bbab82
manjabogicevic <slavo@mimicom24.com>
2018-03-31 00:30:21 -0400

    working code

--- FreeCodeCamp/Basic_alg/ending.js
+++ FreeCodeCamp/Basic_alg/ending.js
@@ -3,7 +3,7 @@
 // This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
 
 function confirmEnding(str, target) {
-  return str;
+  return str.substr(-target.length) === target ? true : false;
 }
 
 console.log(confirmEnding("Bastian", "n"));
--- t10.js
+++ t10.js
@@ -1,3 +1,13 @@
-console.log(`Substring: ${ "slavo".substring(-1, 1)}`);
+console.log(`Substring: ${"slavo".substring(-1, 1)}`);
 
-console.log(`Substr: ${ "slavo".substr(-1, 3)}`);
+console.log(`Substr: ${"slavo".substr(-3)}`);
+
+let name = "Slavoljub";
+
+let end = "pera";
+
+const check = function check(str, target) {
+  return str.substr(-end.length) === end ? true : false;
+};
+
+console.log(check(name, end));
