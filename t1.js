a285cb31a09a04a687db1ec3cb32c43cbbb6a13b
manjabogicevic <slavo@mimicom24.com>
2018-04-01 22:43:00 -0400

    adding

--- t1.js
+++ t1.js
@@ -3,7 +3,20 @@ let num = 11;
 console.log(a);
 console.log(a.slice(0, num));
 console.log("----------------");
+
 let arr = ["a", "b", "c", "d"];
 let arr_new = [];
 
-console.log(arr.slice(0, 2));
+// console.log(arr.slice(0, 2));
+
+for (var i = 0; i < 2; i++) {
+  arr_new.push([]);
+}
+
+console.log(arr_new);
+
+for (var i = 0; i < 2; i++) {
+  arr_new[0].push(arr.slice(0, 2));
+}
+
+console.log(arr_new);
