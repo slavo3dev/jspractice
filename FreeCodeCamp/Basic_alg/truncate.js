ba5ed111b1a90e2472a54d6fc903cdcee3f16606
manjabogicevic <slavo@mimicom24.com>
2018-04-01 20:28:23 -0400

    console.log

--- FreeCodeCamp/Basic_alg/truncate.js
+++ FreeCodeCamp/Basic_alg/truncate.js
@@ -9,12 +9,21 @@ function truncateString(str, num) {
   return str;
 }
 
-truncateString("A-tisket a-tasket A green and yellow basket", 11);
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 11));
 
-
-truncateString("A-tisket a-tasket A green and yellow basket", 11) should return "A-tisket...".
-truncateString("Peter Piper picked a peck of pickled peppers", 14) should return "Peter Piper...".
-truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length) should return "A-tisket a-tasket A green and yellow basket".
-truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2) should return "A-tisket a-tasket A green and yellow basket".
-truncateString("A-", 1) should return "A...".
-truncateString("Absolutely Longer", 2) should return "Ab...".
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 11)); // should return "A-tisket...".
+console.log(truncateString("Peter Piper picked a peck of pickled peppers", 14)); // should return "Peter Piper...".
+console.log(
+  truncateString(
+    "A-tisket a-tasket A green and yellow basket",
+    "A-tisket a-tasket A green and yellow basket".length
+  )
+); // should return "A-tisket a-tasket A green and yellow basket".
+console.log(
+  truncateString(
+    "A-tisket a-tasket A green and yellow basket",
+    "A-tisket a-tasket A green and yellow basket".length + 2
+  )
+); // should return "A-tisket a-tasket A green and yellow basket".
+console.log(truncateString("A-", 1)); // should return "A...".
+console.log(truncateString("Absolutely Longer", 2)); // should return "Ab...".
