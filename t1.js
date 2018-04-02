73bc327758473471ca4b29cb454ac3ad96912842
manjabogicevic <slavo@mimicom24.com>
2018-04-01 22:31:30 -0400

    chunk

--- t1.js
+++ t1.js
@@ -2,3 +2,8 @@ let a = "A-tisket a-tasket A green and yellow basket";
 let num = 11;
 console.log(a);
 console.log(a.slice(0, num));
+console.log("----------------");
+let arr = ["a", "b", "c", "d"];
+let arr_new = [];
+
+console.log(arr.slice(0, 2));
