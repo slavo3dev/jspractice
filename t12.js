2b6e6ae18021755fc69b8704a0d197bd3e639a67
manjabogicevic <slavo@mimicom24.com>
2018-04-03 08:49:04 -0400

    typo

--- t12.js
+++ t12.js
@@ -1,12 +1,16 @@
-let name = "slavo";
+let name = "slavog";
 
 for (var i = 0; i < name.length; i++) {
   if ("moroslav".indexOf(name[i]) === -1) {
-    console.log("You understand this shit");
+    console.log("Nothing");
+    break;
+    console.log("You are Fucked");
   }
 
-  console.log("Nothing");
+  console.log("Its Working");
 }
+
+console.log("miroslav".indexOf("o"));
 
 function mutation(arr) {
   var arr1 = arr[1].toLowerCase();
@@ -22,3 +26,5 @@ function mutation(arr) {
 }
 
 console.log(mutation(["floor", "for"]));
+console.log(mutation(["miroslavilicmiroslav", "slavo"]));
+console.log(mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"])); // should return true.
