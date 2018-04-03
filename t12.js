195932e4e8a6af6cc92fa658e1e255135dfc4e46
manjabogicevic <slavo@mimicom24.com>
2018-04-03 08:36:58 -0400

    return

--- t12.js
+++ t12.js
@@ -1,5 +1,22 @@
-console.log("hey".indexOf("Hello"));
-console.log("Hello".indexOf("Hello"));
-console.log("Alien".indexOf("line"));
+let name = "slavo";
 
-// console.log(["l", "i", "n", "e"].includes[("a", "l", "i", "e", "n")]);
+for (var i = 0; i < name.length; i++) {
+  if ("moroslav".indexOf(name[i] === -1)) {
+    console.log("You understand this shit");
+  }
+
+  console.log("Nothing");
+}
+
+function mutation(arr) {
+  var arr1 = arr[1].toLowerCase();
+  var arr0 = arr[0].toLowerCase();
+
+  for (var i = 0; i < arr1.length; i++) {
+    if (arr[0].indexOf(arr1[i] === -1)) {
+      return false;
+    }
+
+    return true;
+  }
+}
