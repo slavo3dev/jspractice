54c5291996e87864832330361751d1c078d4baa8
manjabogicevic <slavo@mimicom24.com>
2018-04-02 20:02:22 -0400

    array

---
+++ FreeCodeCamp/Basic_alg/flick.js
@@ -0,0 +1,18 @@
+// Return the remaining elements of an array after chopping off n elements from the head.
+
+// The head means the beginning of the array, or the zeroth index.
+
+function slasher(arr, howMany) {
+  if (howMany > arr.length) {
+    return [];
+  } else if (howMany === 0) {
+    return arr;
+  }
+  return arr.slice(howMany);
+}
+
+console.log(slasher([1, 2, 3], 0)); // should return [1, 2, 3].
+console.log(slasher([1, 2, 3], 9)); // should return [].
+console.log(slasher([1, 2, 3], 4)); // should return [].
+console.log(slasher(["burgers", "fries", "shake"], 1)); // should return ["fries", "shake"].
+console.log(slasher([1, 2, "chicken", 3, "potatoes", "cheese", 4], 5)); // should return ["cheese", 4].
