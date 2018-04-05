40e95817026123673ff230c868d977b29cf77903
manjabogicevic <slavo@mimicom24.com>
2018-04-04 21:41:09 -0400

    adding if statement

--- t13.js
+++ t13.js
@@ -1,13 +1,13 @@
 function mutation(arr) {
   let arr0 = arr[0].toLowerCase();
   let arr1 = arr[1].toLowerCase();
-
-  for (var i = 0; i < arr1.length; i++) {
-    if (arr0.indexOf(arr1[i]) === -1) {
-      return false;
-    }
-    return true;
+  console.log(arr0);
+  console.log(arr1);
+  for (i = 0; i < arr1.length; i++) {
+    if (arr0.indexOf(arr1[i]) === -1) console.log(arr.indexOf(arr[i]));
+    return false;
   }
+  return true;
 }
 
 console.log(mutation(["hello", "hey"]));
