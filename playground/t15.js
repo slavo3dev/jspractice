4a507a2f85839fead405de7089527478f9e14655
manjabogicevic <slavo@mimicom24.com>
2018-04-08 22:12:07 -0400

    adding

--- playground/t15.js
+++ playground/t15.js
@@ -2,19 +2,17 @@ let arr = [[1, 2, 3, 4], 1, 2];
 
 let test = arr.slice(1);
 let target = arr[0];
-let newArr = []
+let newArr = [];
+
+console.log(`test ${test}`);
+console.log(`target ${target}`);
 
 for (i = 0; i < test.length; i++) {
-    if (target.indexOf(test[i]) === -1) { newArr.push(i);
+  if (target.indexOf(test[i]) === -1) {
+    newArr.push(arr[i]);
   } else {
-      console.log(`Same Elements + ${i}`)
+    console.log(`Same Elements  ${arr[i]}`);
   }
+}
 
-console.log(test);
-console.log(target);
-
-for (i = 0; i < test.length; i++) {
-    if (target.indexOf(test[i]) === -1) { newArr.push(i);
-  } else {
-      console.log(`Same Elements + ${i}`)
-  }
+console.log(`newArr ${newArr}`);
