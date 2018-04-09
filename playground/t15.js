e3357d94207e785ba091fb4efdef26b44b82a15c
manjabogicevic <slavo@mimicom24.com>
2018-04-08 22:05:24 -0400

    test items

--- playground/t15.js
+++ playground/t15.js
@@ -1,5 +1,20 @@
 let arr = [[1, 2, 3, 4], 1, 2];
 
-let newArr = arr[0].concat(arr);
+let test = arr.slice(1);
+let target = arr[0];
+let newArr = []
 
-console.log(newArr);
+for (i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) === -1) { newArr.push(i);
+  } else {
+      console.log(`Same Elements + ${i}`)
+  }
+
+console.log(test);
+console.log(target);
+
+for (i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) === -1) { newArr.push(i);
+  } else {
+      console.log(`Same Elements + ${i}`)
+  }
