4f05d01428417d658c1fab41ac92e153b7b6ed85
manjabogicevic <slavo@mimicom24.com>
2018-04-16 18:37:56 -0400

    sort function

--- playground/t3.js
+++ playground/t3.js
@@ -1,6 +1,17 @@
-l = [10, 20, 30, 40, 50];
-l1 = [3, 10, 5];
-l2 = [5, 3, 20, 3];
+let l = [10, 20, 30, 40, 50];
+let l1 = [3, 10, 5];
+let l2 = [5, 3, 20, 3];
+let num = 35;
 
-l2.sort();
+l.push(num);
+console.log(
+  l.sort(function(a, b) {
+    return a - b;
+  })
+);
+l1.sort(function(a, b) {
+  return a - b;
+});
+l2.sort((a, b) => a - b);
 console.log(l2);
+console.log(l.indexOf(num));
