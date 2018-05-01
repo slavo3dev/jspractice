5cafddc889dcceabb15d906dce8d4bce9f6d04f0
Biljana Bozic <slavo@mimicom24.com>
2018-05-01 19:54:31 -0400

    new arr

--- wars/wars7.js
+++ wars/wars7.js
@@ -6,7 +6,8 @@ var maxSequence = function(arr) {
   }
 
   let nArr = arr.filter(x => x > 0);
-  return nArr.reduce((a, b) => a + b);
+  let arr_new = nArr.filter((a,b) => nArr.indexOf(a) === b);
+  return arr_new.reduce((a, b) => a + b);
 };
 
 console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
