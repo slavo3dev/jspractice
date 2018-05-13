007b25d213c548160cd042071139c2572128de72
Biljana Bozic <slavo@mimicom24.com>
2018-05-13 15:30:12 -0400

    tabs

--- wars/wars8.js
+++ wars/wars8.js
@@ -1,13 +1,4 @@
-// Given a string of words, you need to find the highest scoring word.
+// The main idea is to count all the occuring characters(UTF-8) in string.
+//  If you have string like this aba then the result should be { 'a': 2, 'b': 1 }
 
-// Each letter of a word scores points according to it's position in the alphabet: a = 1, b = 2, c = 3 etc.
-
-// You need to return the highest scoring word as a string.
-
-// If two words score the same, return the word that appears earliest in the original string.
-
-// All letters will be lowercase and all inputs will be valid.
-
-function high(x) {
-    xString = x.toLowerCase();
-}
+// What if the string is empty ? Then the result should be empty object literal { }
