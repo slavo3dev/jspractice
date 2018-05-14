d80d7fad7c68d15af7b669c806b7b9f407ec57f8
manjabogicevic <slavo@mimicom24.com>
2018-05-14 16:16:01 -0400

    adding simple example

---
+++ js_file/js/main.js
@@ -0,0 +1,7 @@
+var MyAccBalance = 300;
+var nikkeSBShoes = 45.56;
+if (nikkeSBShoes <= MyAccBalance) {
+    MyAccBalance -= nikkeSBShoes
+    console.log('We just both sine nice shoes')
+    console.log('Acc Balance ' + MyAccBalance);
+}
