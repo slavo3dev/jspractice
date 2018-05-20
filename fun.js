8a6df8d01b762b48f0718e8ee67c871ee0138232
slavo7 <slavo@mimicom24.com>
2018-05-19 21:42:42 -0400

    first func

---
+++ fun.js
@@ -0,0 +1,7 @@
+// Write a function identity that takes an argument and returns that argument
+const identity = function identity(x) {
+    return x
+}
+
+console.log(identity('Hello Slavo'))
+console.log('---------------------')
