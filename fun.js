96648da21d6a99972815ad43a841ffae5a08e42b
slavo7 <slavo@mimicom24.com>
2018-05-19 21:44:18 -0400

    2nd func

--- fun.js
+++ fun.js
@@ -2,6 +2,13 @@
 const identity = function identity(x) {
     return x
 }
-
 console.log(identity('Hello Slavo'))
 console.log('---------------------')
+
+// Write a binary function addb that takes two numbers and returns their sum
+
+const addb = (a, b) => {
+    return a + b
+}
+console.log(addb(3, 4))
+console.log('-----------------------')
