0917b9f774fe56c7dac19e788f98dfce4050690e
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-29 19:25:40 -0400

    great example

---
+++ EnkiBot/3t.js
@@ -0,0 +1,13 @@
+let arr = [111,2345,66,78,900]; 
+let arrStr = [];
+var newArr = []
+
+for (let i = 0; i < arr.length; i++){
+    arrStr.push(arr[i].toString());
+}
+
+arrStr = arrStr.map(x => x.split(''));
+
+arrStr = 
+
+console.log(arrStr);
