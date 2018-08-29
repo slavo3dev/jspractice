d6e9e91ea47a2e5c428b0ea17c13c6aee3a9bfb2
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-29 00:49:25 -0400

    making dict

--- EnkiBot/2.js
+++ EnkiBot/2.js
@@ -20,13 +20,17 @@ Grade X: score=-1(The cheating guy gets a score like that)
 
 function countGrade(scores){
     //coding here...
-    let grade = [S, A, B, C, D, X]
-    let gradeResult = {}
-    
-    for(let i = 0; i < scores.legth; i++){
-         gradeResult[scores[i]] = i;
+
+    let gradeResult = {
+        S: scores.filter(x=> x === 100).length,
+        A: scores.filter(x=> x < 100 && x >= 90).length,
+        B: scores.filter(x=> x < 90 && x >= 80).length, 
+        C: scores.filter(x=> x < 80 && x >= 60).length,
+        D: scores.filter(x=> x<60 && x >= 0).length,
+        X: scores.filter(x=>  x === -1).length
     }
-   return gradeResult
+      
+    return gradeResult
   }
 
 
@@ -34,7 +38,7 @@ function countGrade(scores){
 
 console.log(countGrade([50,60,70,80,90,100]))
 // should return {S:1, A:1, B:1, C:2, D:1, X:0}
-console.log(countGrade([65,75,,85,85,95,100,100]))
+console.log(countGrade([65,75,85,85,95,100,100]))
 // should return {S:2, A:1, B:2, C:2, D:0, X:0}
 console.log(countGrade([-1,-1,-1,-1,-1,-1]))
 // should return {S:0, A:0, B:0, C:0, D:0, X:6}
