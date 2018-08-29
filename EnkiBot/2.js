3a820fd2d390f4eb875c28535acc0d90a9372825
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-29 00:19:42 -0400

    adding new problem

---
+++ EnkiBot/2.js
@@ -0,0 +1,6 @@
+// This lesson we learn a very useful method of array: filter(). The filter() method creates a new array with all elements that pass the test implemented by the provided function. For more information, please refer to: Array.prototype.filter()
+
+// Here are some examples to help us understand the use of filter():
+
+// filter() always accept a function as parameter, the function always return boolean value true or false. filter() returns a new array that contains only the elements that meet the conditions(The elements as a parameters of the function, return true value).
+
