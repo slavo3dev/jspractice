fc4fb90faf6034a717d4e4513960f9de64b128a5
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-29 00:32:00 -0400

    making simple obj from array

--- EnkiBot/2.js
+++ EnkiBot/2.js
@@ -22,17 +22,19 @@ function countGrade(scores){
     //coding here...
     let grade = [S, A, B, C, D, X]
     let gradeResult = {}
-
     
-
+    for(let i = 0; i < scores.legth; i++){
+         gradeResult[scores[i]] = i;
+    }
+   return gradeResult
   }
 
 
 // Some example:
 
-countGrade([50,60,70,80,90,100]) 
+console.log(countGrade([50,60,70,80,90,100]))
 // should return {S:1, A:1, B:1, C:2, D:1, X:0}
-countGrade([65,75,,85,85,95,100,100]) 
+console.log(countGrade([65,75,,85,85,95,100,100]))
 // should return {S:2, A:1, B:2, C:2, D:0, X:0}
-countGrade([-1,-1,-1,-1,-1,-1]) 
+console.log(countGrade([-1,-1,-1,-1,-1,-1]))
 // should return {S:0, A:0, B:0, C:0, D:0, X:6}
