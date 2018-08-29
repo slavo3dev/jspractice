511c243235cee32a172944239844224ef5aa9ded
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-29 00:25:30 -0400

    scores

--- EnkiBot/2.js
+++ EnkiBot/2.js
@@ -17,3 +17,18 @@ Grade D: score<60 and score>=0
 Grade X: score=-1(The cheating guy gets a score like that)
 
 */
+
+function countGrade(scores){
+    //coding here...
+    
+  }
+
+
+// Some example:
+
+countGrade([50,60,70,80,90,100]) 
+// should return {S:1, A:1, B:1, C:2, D:1, X:0}
+countGrade([65,75,,85,85,95,100,100]) 
+// should return {S:2, A:1, B:2, C:2, D:0, X:0}
+countGrade([-1,-1,-1,-1,-1,-1]) 
+// should return {S:0, A:0, B:0, C:0, D:0, X:6}
