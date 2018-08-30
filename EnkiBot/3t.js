d4d5b7589caac5af338864de089633d280c313b3
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-29 22:47:43 -0400

    try

--- EnkiBot/3t.js
+++ EnkiBot/3t.js
@@ -1,9 +1,10 @@
 let arr = [111,2345,66,78,900]; 
+let arr1 = [1,2,3,4,5]
 let arrStr = [];
-var newArr = []
+let newArr = []
 
-for (let i = 0; i < arr.length; i++){
-    arrStr.push(arr[i].toString());
+for (let i = 0; i < arr1.length; i++){
+    arrStr.push(arr1[i].toString());
 }
 
 arrStr = arrStr.map(x => x.split(''));
@@ -15,15 +16,17 @@ for(var i = 0; i < arrStr.length; i++){
         console.log('hello 1')
     } else if ( i === arrStr.length - 1 ){
         arrStr[i].splice(1)
-    } else if (arrStr[i].length === 2  || arrStr[i].length === 1) {
+    } else if (arrStr[i].length === 2) {
         continue
+    } else if (arrStr[i].length === 1){
+           arrStr[i].push(arrStr[i][0])
     } else {
         arrStr[i].splice(1, arrStr[i].length - 2)
     }
 }
-
+console.log("-------Filtred Arr-------")
 console.log(arrStr);
-
+console.log("---------New Arr---------")
 newArr = arrStr.reduce((a,b) => a.concat(b))
 
 console.log(newArr)
@@ -39,6 +42,15 @@ let chankArr = newArr.reduce((resultArray, item, index) => {
   
     return resultArray
   }, [])
-console.log("----------------------")
+console.log("-------Chunk Arr-------")
 
 console.log(chankArr)
+
+console.log('-------- Count Arr -----------' )
+
+let accountArr = chankArr.map(x => x.reduce((a,b) => a + b));
+console.log(accountArr)
+
+let result = accountArr.reduce((a,b) => a*b)
+console.log('-------- Result ----------------')
+console.log(result)
