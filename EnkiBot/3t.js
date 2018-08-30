79f258e8b764617332816f9f54bc5880bf1ff6a5
slavo@mimicom24.com <slavo@mimicom24.com>
2018-08-29 22:21:03 -0400

    filter func

--- EnkiBot/3t.js
+++ EnkiBot/3t.js
@@ -11,13 +11,18 @@ arrStr = arrStr.map(x => x.map(y => parseInt(y)));
 
 for(var i = 0; i < arrStr.length; i++){
     if (i === 0){
-        arrStr[i].splice(-1)
+        arrStr[i].splice(0, arrStr[i].length - 1)
         console.log('hello 1')
     } else if ( i === arrStr.length - 1 ){
         arrStr[i].splice(1)
+    } else if (arrStr[i].length === 2  || arrStr[i].length === 1) {
+        continue
+    } else {
+        arrStr[i].splice(1, arrStr[i].length - 2)
+    }
 }
-}
-
 
 console.log(arrStr);
+
+
 console.log(newArr)
