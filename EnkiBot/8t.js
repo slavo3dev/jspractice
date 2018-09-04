c29b7d5c4866d49928b471f85ba0cc5e246c0997
slavo@mimicom24.com <slavo@mimicom24.com>
2018-09-04 16:03:48 -0400

    binding

---
+++ EnkiBot/8t.js
@@ -0,0 +1,15 @@
+var func = function () {
+    return this.prop;
+};
+var obj1 = {
+        prop: 1
+    },
+    obj2 = {
+        prop: 2
+    };
+
+func = func.bind(obj1);
+console.log(func()); // Will produce 1
+
+func = func.bind(obj2);
+console.log(func()); // Will also produce 1
