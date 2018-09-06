bf4a733c65cbb7159b1894d52b41379e044d3469
slavo@mimicom24.com <slavo@mimicom24.com>
2018-09-06 00:51:05 -0400

    trying to make

--- EnkiBot/8t.js
+++ EnkiBot/8t.js
@@ -2,5 +2,19 @@ let str = "My name is John";
 
 let arr = str.split(" ")
 console.log(arr);
-arr = arr.map(x => x.split(" "))
+arr = arr.map(x => x.split(""))
 console.log(arr)
+
+
+
+for (let i = 0; i < arr.length; i++) {
+    for (let x = 0; x < arr[i].length; x++) {
+        if (x === (arr[i].length - 1)) {
+            n = n + arr[i][x].toLowerCase()
+        } else {
+            n = "" + arr[i][x].toUpperCase();
+        }
+    }
+}
+
+console.log(n);
