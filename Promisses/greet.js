5821d01e57af0e71760602eb88f356fc893e7508
slavo@mimicom24.com <slavo@mimicom24.com>
2018-09-12 17:20:05 -0400

    finally understand

--- Promisses/greet.js
+++ Promisses/greet.js
@@ -1,10 +1,21 @@
-let greet = new Promise(function (resolve, reject) {
+let number = 10;
 
-    resolve("Hello Slavo Finnaly");
-
-}).then(function () {
-    return console.log('I am going first');
+let test = new Promise(function (resolve, reject) {
+    let myNumber = 10;
+    if (number === myNumber) {
+        resolve();
+    } else {
+        reject()
+    }
+    // resolve('Hello Slavo');
 })
 
-// console.log(typeof (greet));
-console.log(greet);
+
+
+// console.log(test);
+
+test.then(function () {
+    console.log('Hello Slavo!!!!!')
+}).catch(function () {
+    console.log('Wrong Number baby')
+});
