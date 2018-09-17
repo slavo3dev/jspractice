786891551ca7529268672f9051fed0ec1341efef
slavo@mimicom24.com <slavo@mimicom24.com>
2018-09-16 22:17:32 -0400

    object literal

---
+++ ES6/objectLiteral.js
@@ -0,0 +1,14 @@
+/* 
+Object literal shorthand
+You’ve probably written code where an object is being initialized using the same property names as the variable names being assigned to them.
+
+
+*/
+
+let gemstone = {
+    type,
+    color,
+    carat,
+    //calculateWorth() { ...
+    // }
+};
