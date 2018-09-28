9784c1f099b57236a61116aaa534ffb248970efe
slavo@mimicom24.com <slavo@mimicom24.com>
2018-09-27 23:55:16 -0400

    adding 1,2,3

--- EnkiBot/14.js
+++ EnkiBot/14.js
@@ -7,4 +7,4 @@ console.log(enki[enkiIterator.next().value])
 console.log('---------')
 
 let ints = ['1','2','3','4','5','6','7'];
-console.log(ints.copyWithin(7, 3, 8));
+console.log(ints.copyWithin(1,2,3 )); // ['7','3','4','5','6','7']
