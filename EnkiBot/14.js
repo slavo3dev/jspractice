6a5256a11520843020e3f2a64259913d3c1f47ce
slavo@mimicom24.com <slavo@mimicom24.com>
2018-09-28 00:46:11 -0400

    prcitcing javascript

--- EnkiBot/14.js
+++ EnkiBot/14.js
@@ -9,3 +9,9 @@ console.log('---------')
 let ints = ['1','2','3','4','5','6','7'];
 console.log(ints.copyWithin(1,6,7)); // ['7','3','4','5','6','7']
 console.log(ints.copyWithin(1,2,3));
+
+console.log('--------------')
+let ints1 = ['1','2','3','4','5','6'];
+console.log(ints1.copyWithin(1,2,3));
+// console.log(ints1.copyWithin(1,5));  //['1','2','1','2','5','6']
+ //['1','2','1','2','5','6']
