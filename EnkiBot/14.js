cdd63888c4886e247d6cf5fe1710c6f90fbac46b
slavo@mimicom24.com <slavo@mimicom24.com>
2018-09-27 23:45:20 -0400

    testing

--- EnkiBot/14.js
+++ EnkiBot/14.js
@@ -3,3 +3,8 @@ var enki = ["e", "n", "k", "i"]
 var enkiIterator = enki.keys()
 
 console.log(enki[enkiIterator.next().value])
+
+console.log('---------')
+
+let ints = ['1','2','3','4','5','6','7'];
+console.log(ints.copyWithin(7, 3, 8));
