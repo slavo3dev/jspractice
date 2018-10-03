71f731a946819d8b96f455222af32fa5868d97f6
slavo@mimicom24.com <slavo@mimicom24.com>
2018-10-03 18:52:58 -0400

    adding new project

---
+++ wars/wars11.js
@@ -0,0 +1,11 @@
+/* 
+"7777...8?!??!", exclaimed Bob, "I missed it again! Argh!" Every time there's an interesting number coming up, he notices and then promptly forgets. Who doesn't like catching those one-off interesting mileage numbers?
+
+Let's make it so Bob never misses another interesting number. We've hacked into his car's computer, and we have a box hooked up that reads mileage numbers. We've got a box glued to his dash that lights up yellow or green depending on whether it receives a 1 or a 2 (respectively).
+
+It's up to you, intrepid warrior, to glue the parts together. Write the function that parses the mileage number input, and returns a 2 if the number is "interesting" (see below), a 1 if an interesting number occurs within the next two miles, or a 0 if the number is not interesting.
+*/
+
+function isInteresting(number, awesomePhrases) {
+    // Go to town!
+  }
